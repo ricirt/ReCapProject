fbf9ca6d4cb1ab0cfa545f98c1b537e5a4071152
fatih <fatih@DESKTOP-PT432LC>
2021-07-28 15:11:28 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager manager = new CarManager(new InMemoryCarDal());
+            list(manager);
+            manager.Add(new Car { Id = 4, BrandId = 3, ColorId = 3, DailyPrice = 120, Description = "AddDescription", ModelYear = 2016 });
+            list(manager);
+            manager.Update(new Car { Id = 4, ModelYear = 2019 });
+            list(manager);
+        }
+        private static void list(CarManager manager)
+        {
+            foreach (var car in manager.GetAll())
+            {
+                Console.WriteLine("ID " + car.Id + "  YEAR " + car.ModelYear);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,80 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car
+                {
+                    Id=1,
+                    BrandId=1,
+                    ColorId=1,
+                    DailyPrice=20,
+                    Description="Araba Kiralıktır1",
+                    ModelYear=1998
+                },
+                 new Car
+                {
+                    Id=2,
+                    BrandId=1,
+                    ColorId=2,
+                    DailyPrice=10,
+                    Description="Araba Kiralıktır2",
+                    ModelYear=2021
+                },               
+                new Car
+                {
+                    Id=3,
+                    BrandId=2,
+                    ColorId=1,
+                    DailyPrice=100,
+                    Description="Araba Kiralıktır3",
+                    ModelYear=2012
+                }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete;
+            carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            Car car = _cars.SingleOrDefault(c => c.Id == id);
+            return car;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+    }
+}

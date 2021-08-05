224756b19afd67ec53e8ee9e572b709326fc2844
fatih <fatih@DESKTOP-PT432LC>
2021-08-05 12:01:50 +0300

    Security entityleri oluşturuldu.

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -22,9 +23,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(u => u.LastName).MinimumLength(3);
             RuleFor(u => u.LastName).NotEmpty();
             RuleFor(u => u.LastName).NotNull();
-            RuleFor(u => u.Password).NotNull();
-            RuleFor(u => u.Password).NotEmpty();
-            RuleFor(u => u.Password).MinimumLength(6);
+
 
             
             
